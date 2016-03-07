041d650365edfe58ed092e0d81b99988aaa1efa1
szymonlipka <revan12311@gmail.com>
2016-03-07 16:26:55 +0100

    Seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)s
-pokemon1 = Pokemon.new( first_name: 'Pika', last_name: 'Chu' )
-pokemon2 = Pokemon.new( first_name: 'Snor', last_name: 'Lax' )
-pokemon1.save
-pokemon2.save
+pokemon1 = Pokemon.create( first_name: 'Pika', last_name: 'Chu', learning: 5, inference: 4, agility: 1)
+pokemon2 = Pokemon.create( first_name: 'Snor', last_name: 'Lax', flying: 4, speed: 4, strength: 2 )
 pokemon1.calculate_battle_points
 pokemon2.calculate_battle_points
