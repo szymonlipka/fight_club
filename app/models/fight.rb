2e654eadd647d64fc7247c6470e41b49fb70d61a
szymonlipka <revan12311@gmail.com>
2016-03-07 15:57:34 +0100

    Fights integration test passing

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -15,8 +15,8 @@ class Fight < ActiveRecord::Base
     # points and looser has 1000 points gained by winner are 10 and the same amount loosing looser
     ranking_points = 20 - (winner.ranking_points - looser.ranking_points)/100.to_f
     # Experience gained by winning or loosing is by default 50 and 25 but Learing and Inference skills can make is higher 
-    (winner.skills[5].value == 0) ? (winning_exp = winner.experience + 50) : (winning_exp = winner.experience + (50 * (winner.skills[5].value * 2)))
-    (looser.skills[6].value == 0) ? (loosing_exp = looser.experience + 50) : (loosing_exp = looser.experience + (50 * (looser.skills[6].value * 2)))
+    (winner.learning == 0) ? (winning_exp = winner.experience + 50) : (winning_exp = winner.experience + (50 * (winner.learning * 2)))
+    (looser.inference == 0) ? (loosing_exp = looser.experience + 50) : (loosing_exp = looser.experience + (50 * (looser.inference * 2)))
     winner.update_attributes(experience: winning_exp, fights_won: winner.fights_won + 1, ranking_points: winner.ranking_points + ranking_points)
     looser.update_attributes(experience: loosing_exp, fights_lost: looser.fights_lost + 1, ranking_points: looser.ranking_points - ranking_points)
   end
--- spec/features/fights_spec.rb
+++ spec/features/fights_spec.rb
@@ -1,21 +1,23 @@
 require "rails_helper"
 
 describe  'adding fights' do
-  let(:pokemon){create(:pokemon)}
-  let(:pokemon1){create{:pokemon1}}
   it 'is possible' do
+    pokemon = Pokemon.create(first_name: "MyString", last_name: "MyString", learning: 5, inference: 4, agility: 1)
+    pokemon1 = Pokemon.create(first_name: "MyString1", last_name: "MyString1", flying: 4, speed: 4, strength: 2)
+    pokemon.calculate_battle_points
+    pokemon1.calculate_battle_points
     visit new_fight_path
     print page.html
     find('#fight_pokemon_id').find(:xpath, 'option[1]').select_option
     find('#fight_competitor_id').find(:xpath, 'option[2]').select_option
     click_button 'FIGHT!'
     expect(page).to have_content Pokemon.last.full_name + ' won'
-    expect(pokemon.fights_lost).to eq(1)
-    expect(pokemon1.fights_won).to eq(1)
-    expect(pokemon.experience).to eq(50)
-    expect(pokemon1.experience).to eq(100)
-    expect(pokemon1.ranking_points).to eq(1020)
-    expect(pokemon.ranking_points).to eq(980)
+    expect(Pokemon.first.fights_lost).to eq(1)
+    expect(Pokemon.last.fights_won).to eq(1)
+    expect(Pokemon.first.experience).to eq(400)
+    expect(Pokemon.last.experience).to eq(50)
+    expect(Pokemon.last.ranking_points).to eq(1020)
+    expect(Pokemon.first.ranking_points).to eq(980)
     expect(Fight.last.winner).to eq(pokemon1)
     expect(Fight.last.looser).to eq(pokemon)
   end
